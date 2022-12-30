5eef8e72b431621dff51b1202fd22d35b173bdfe
Gaurav Joshi <2020uee1211@mnit.ac.in>
2022-12-31 02:19:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const https=  require("https"); 
+const app= express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.get("/",function(req,res){
+	res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){ 
+	const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+	const email = req.body.eMail;
+	const password = req.body.password;
+	const data = {
+		members:[
+		{
+			email_address:email,
+			status: "subscribed",
+			merge_fields: {
+				FNAME: firstName,
+				LNAME: lastName,
+				PASSWORD: password
+			}
+		}
+			]
+	};
+	var jsonData = JSON.stringify(data);
+	const options = {
+		method:"post",
+		auth: "gaurav1211:process.env.API_KEY",
+	};
+
+	const url = "https://us10.api.mailchimp.com/3.0/lists/process.env.56761ccbf4";
+	
+	const request = https.request(url,options,function (response) {
+		if(response.statusCode===200){
+			res.sendFile(__dirname+"/success.html");
+			
+		}else{
+			res.sendFile(__dirname+"/failure.html");
+			
+		}
+		response.on("data",function(data){
+			console.log(JSON.parse(data));
+		});
+	}); 
+	request.write(jsonData);
+	request.end();
+	});
+	app.post("/failure",function(req,res){
+		res.redirect("/");
+	});
+
+
+app.listen(process.env.PORT || 3000,function(){
+	console.log("server is running at port 8000");
+})
+
+  
+// api KEY = 8c16dc092b2a383235a66e2c77581ec8-us10
+// LIST ID = 56761ccbf4
